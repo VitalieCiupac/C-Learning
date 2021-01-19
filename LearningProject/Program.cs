c7ac75a5205f6b00b0ea465a34ddec7bcd21a022
CiupacVitalie <39079415+CiupacVitalie@users.noreply.github.com>
2021-01-19 23:14:00 +0200

    LP
    d2

--- LearningProject/Program.cs
+++ LearningProject/Program.cs
@@ -8,10 +8,34 @@ namespace LearningProject
         {
             string nameValue = "John";
             int ageValue = 35;
+            char firstLetter = 'V';
+
+            double ag = 9.8;
+
+            Console.WriteLine("ABC\nAFS\"D");
+            Console.WriteLine(nameValue.IndexOf("gf"));
+            Console.WriteLine(-98.376);
+
+            Console.WriteLine(7 + 2);
+            Console.WriteLine(7 * 2);
+
+            Console.WriteLine(ageValue += 1);
+            Console.WriteLine(Math.Pow(3, 2));
+            Console.WriteLine(Math.Sqrt(49));
+            Console.WriteLine(Math.Round(4.6));
+
+            
+            
+
+            
 
             Console.WriteLine("His name is: " + nameValue);
             Console.WriteLine("His age is: " + ageValue);
-            Console.WriteLine("Hello World!");
+            ageValue = 25;
+            Console.WriteLine("His age is: " + ageValue);
+            Console.WriteLine("First character: "+ firstLetter);
+            Console.WriteLine("Acceleratia gravitationala este egala cu: " + ag);
+           
             Console.ReadLine();
         }
     }
