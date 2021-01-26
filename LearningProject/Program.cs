ea643d9a907aa95b06ee2ffcb8447c67acbbe013
CiupacVitalie <39079415+CiupacVitalie@users.noreply.github.com>
2021-01-26 22:00:05 +0200

    LP
    d5

--- LearningProject/Program.cs
+++ LearningProject/Program.cs
@@ -6,21 +6,9 @@ namespace LearningProject
     {
         static void Main(string[] args)
         {
-            SayHello("Vitalie");
-            Console.WriteLine(CubeNumber(5));
-            bool isMale = true;
-            bool istall = true;
-
-            if (isMale is false || istall)
-            {
-                SayHello("male");
-            } else
-            {
-                SayHello("not male.");
-            }
+            whileMeth("It's ok", 5);
             Console.ReadLine();
         }
-
         static void SayHello(string name)
         {
             Console.WriteLine("Hi friend "+ name);
@@ -31,5 +19,64 @@ namespace LearningProject
             int result = numberToCube * numberToCube * numberToCube;
             return result;
         }
+
+        static int GetMax(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
+        }
+
+        static string SwitchMet(int dayNum)
+        {
+            string dayName;
+
+            switch (dayNum)
+            {
+                case 0:
+                    dayName = "Monday";
+                    break;
+                case 1:
+                    dayName = "Tuesday";
+                    break;
+                case 2:
+                    dayName = "Wednesday";
+                    break;
+                case 3:
+                    dayName = "Thursday";
+                    break;
+                case 4:
+                    dayName = "Friday";
+                    break;
+                case 5:
+                    dayName = "Saturday";
+                    break;
+                case 6:
+                    dayName = "Sunday";
+                    break;
+                default:
+                    dayName = "Invalid number";
+                    break;
+            }
+
+            return dayName;
+        }
+
+        static void whileMeth(string word, int times)
+        {
+            int index = 1;
+            while (index <= times)
+            {
+                Console.WriteLine(word);
+                index++;
+            }
+
+        }
+
     }
 }
